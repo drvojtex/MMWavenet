d3a53ef3e8f060741e22e951a053164b57af0fb1
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-02-08 13:34:14 +0100

    Update dataset.jl

--- dataset.jl
+++ dataset.jl
@@ -7,10 +7,10 @@ clean_path = "/largeandslow/kozel/dataset/cleanwares/"
 
 function make_dataset(path::String)
     names::Array{String} = readdir(path)
-    dataset::Array{Float64, 3} = zeros(15, 1024, 1)
+    dataset::Array{Float64, 3} = zeros(4, 256, 1)
     for name::String in names
         d::Matrix{Float64} = hcat(Pickle.load(open(join([path, name])))...)
-        dataset = cat(dataset, d; dims=3)
+        dataset = cat(dataset, d[:, 1:256, :]; dims=3)
     end
     return dataset[:,:,2:end];
 end
@@ -18,4 +18,4 @@ end
 malwares = make_dataset(mal_path)
 cleanwares = make_dataset(clean_path)
 
-bson("/largeandslow/kozel/dataset/dataset.bson", m = malwares, c = cleanwares)
+bson("/largeandslow/kozel/dataset/dataset_s.bson", m = malwares, c = cleanwares)
