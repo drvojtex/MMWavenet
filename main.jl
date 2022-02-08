c5f2eb7d8315ce9f8541a86ef10774c5d7f5b82f
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-02-08 13:33:42 +0100

    Update main.jl

--- main.jl
+++ main.jl
@@ -4,20 +4,18 @@ using Shuffle
 
 include("network.jl")
 
+# dataset setup
 sample_len = 256;
-sample_features = 15;
+sample_features = 4;
 class_no = 1;
-#samples_cnt = 100;
-#data = randn(Float32, sample_len, sample_features, samples_cnt);
-#labels = rand(0:class_no, samples_cnt);
+dataset_split = 0.8
 
-dataset = BSON.load("/largeandslow/kozel/dataset/dataset.bson")
+dataset = BSON.load("/largeandslow/kozel/dataset/dataset_s.bson")
 dataset[:m] = permutedims(dataset[:m], (2,1,3))
 dataset[:m] = dataset[:m][:,:,shuffle(1:end)]
 dataset[:c] = permutedims(dataset[:c], (2,1,3))
 dataset[:c] = dataset[:c][:,:,shuffle(1:end)]
 
-dataset_split = 0.9
 trn_mal = dataset[:m][:,:,1:Int64(floor(dataset_split*size(dataset[:m])[3]))]
 val_mal = dataset[:m][:,:,Int64(floor(dataset_split*size(dataset[:m])[3])):end]
 trn_clean = dataset[:c][:,:,1:Int64(floor(dataset_split*size(dataset[:m])[3]))]
@@ -31,12 +29,14 @@ val_labels = Vector{Int64}(cat(ones(size(val_mal)[3]), zeros(size(val_clean)[3])
 trn_dataset = (trn_data, trn_labels) 
 val_dataset = (val_data, val_labels)
 
-wavenet, θ = build_wavenet(sample_features, sample_len, kernel_size=2, out_dim=class_no);
+wavenet, θ = build_wavenet(sample_features, sample_len, kernel_size=1, out_dim=class_no);
 
-train!(θ, trn_dataset, val_dataset, iter=1000)
+train!(θ, trn_dataset, val_dataset, iter=200)
 
-print_conmat(Vector{Int64}((wavenet(val_data) .>= 0.5)'[:,1]), Vector{Int64}(val_labels), true)
-print_conmat(Vector{Int64}((wavenet(val_data) .>= 0.5)'[:,1]), Vector{Int64}(val_labels), false)
+print_conmat(Vector{Float64}(wavenet(val_data)'[:,1]), Vector{Int64}(val_labels), true)
+print_conmat(Vector{Float64}(wavenet(val_data)'[:,1]), Vector{Int64}(val_labels), false)
 
 #using Plots
-#rocplot(labels, Vector{Float64}((wavenet(data)[1,:])))
+#rocplot(val_labels, Vector{Float64}((wavenet(val_data)[1,:])))
+
+bson("targets_scores_s.bson", Dict(:t=>Vector{Int64}(val_labels), :s=>Vector{Float64}(wavenet(val_data)'[:,1])))
