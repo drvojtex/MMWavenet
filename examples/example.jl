1de132afc3d360612a4179690b14fbfd071c563a
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-02-09 14:32:15 +0100

    added example & repaired some bugs

--- examples/example.jl
+++ examples/example.jl
@@ -1,6 +1,10 @@
 
-import Pkg; Pkg.update()
+import Pkg
+Pkg.activate(normpath(joinpath(@__DIR__, ".")))
+Pkg.update()
 
 using MMWavenet
 
-network, ps = main(256, 4, "random_data.bson")
+println("Learn MMWavenet on random dateset.")
+
+main(256, 4, "random_data.bson");
